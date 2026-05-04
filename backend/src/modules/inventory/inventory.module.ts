9aea4a0c0b50d320a66d5d95cc1cd19c9cf2d077
celalkale-gitcode <celalkale@gmail.com>
2026-05-05 01:12:50 +0300

    Add Inventory module with controller and service

--- backend/src/modules/inventory/inventory.module.ts
+++ backend/src/modules/inventory/inventory.module.ts
@@ -1,1 +1,11 @@
+import { Module } from '@nestjs/common';
+import { InventoryService } from './inventory.service';
+import { InventoryController } from './inventory.controller';
+import { PrismaService } from '../../core/database/prisma.service';
+
+@Module({
+  controllers: [InventoryController],
+  providers: [InventoryService, PrismaService],
+})
+export class InventoryModule {}
 
