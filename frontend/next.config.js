43f888e968b0b20080756b16630a244922d37614
celalkale-gitcode <celalkale@gmail.com>
2026-05-06 02:36:36 +0300

    Update and rename next.config.ts to next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
