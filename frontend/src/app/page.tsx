e067a2c3b5616f7d2b2261e984546a955f9119d8
celalkale-gitcode <celalkale@gmail.com>
2026-05-06 21:04:32 +0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,1 +1,11 @@
+import Link from "next/link";
 
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Ana Sayfa</h1>
+
+      <Link href="/dashboard">Dashboard'a Git</Link>
+    </div>
+  );
+}
