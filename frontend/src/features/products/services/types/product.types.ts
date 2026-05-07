fb08f4b5ec0cc00819d16909450607362f8c8980
celalkale-gitcode <celalkale@gmail.com>
2026-05-08 01:42:12 +0300

    Update product.types.ts

--- frontend/src/features/products/services/types/product.types.ts
+++ frontend/src/features/products/services/types/product.types.ts
@@ -1,1 +1,28 @@
+export interface Product {
+  id: string;
 
+  modelAdi: string;
+
+  modelKodu: string;
+
+  marka?: string;
+
+  varyantlar: ProductVariant[];
+}
+
+export interface ProductVariant {
+  id: string;
+
+  renk: string;
+
+  beden: string;
+
+  sku: string;
+
+  stokMiktari: number;
+
+  barkodlar: {
+    id: string;
+    barkod: string;
+  }[];
+}
