ba9face65e9ee4419b6ef189a8c5a93297cd05b2
celalkale-gitcode <celalkale@gmail.com>
2026-05-08 01:10:55 +0300

    Refactor InventoryModule to include ProductsModule

--- backend/src/modules/inventory/inventory.module.ts
+++ backend/src/modules/inventory/inventory.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+
 import { PrismaService } from '../../core/database/prisma.service';
 
+import { ProductsModule } from '../products/products.module';
+
 @Module({
-  controllers: [InventoryController],
-  providers: [InventoryService, PrismaService],
+  imports: [
+    ProductsModule,
+  ],
+
+  controllers: [
+    InventoryController,
+  ],
+
+  providers: [
+    InventoryService,
+    PrismaService,
+  ],
 })
 export class InventoryModule {}
-
-
