34018a1d48413835c8a368e21fb1a1ff08a7cf98
celalkale-gitcode <celalkale@gmail.com>
2026-05-17 20:35:11 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/products/products.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
+import { LocationModule } from './modules/location/location.module'; // YENİ EKLENDİ
 
 @Module({
-  imports: [ProductsModule, InventoryModule],
+  imports: [
+    ProductsModule, 
+    InventoryModule, 
+    LocationModule, // YENİ EKLENDİ: Konum ve Raf yönetim sistemi ana modüle bağlandı
+  ],
 })
 export class AppModule {}
-
-
-
