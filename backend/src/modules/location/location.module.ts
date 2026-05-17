3ddb7c8de1b7a991cdbdb13039354001df37e4d8
celalkale-gitcode <celalkale@gmail.com>
2026-05-17 20:33:17 +0300

    Update location.module.ts

--- backend/src/modules/location/location.module.ts
+++ backend/src/modules/location/location.module.ts
@@ -1,1 +1,11 @@
+import { Module } from '@nestjs/common';
+import { LocationService } from './location.service';
+import { LocationController } from './location.controller';
+import { PrismaService } from '../../core/database/prisma.service';
 
+@Module({
+  controllers: [LocationController],
+  providers: [LocationService, PrismaService],
+  exports: [LocationService], // Gerekirse başka modüller de raflara erişebilsin
+})
+export class LocationModule {}
